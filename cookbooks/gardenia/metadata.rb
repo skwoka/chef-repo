e28f6cc55824a1e8325093d5e12bda3cb43a8b86
Stanley Kwok <skwoka@gmail.com>
2014-01-07 21:00:43 -0800

    Declared cookbook dependencies

--- cookbooks/gardenia/metadata.rb
+++ cookbooks/gardenia/metadata.rb
@@ -1,7 +1,10 @@
 name             'gardenia'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Gardenia Software'
+maintainer_email 'skwoka@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures gardenia'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends          'monit'
+depends          'build-essential'
